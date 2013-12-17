9ab7998f1c8f98dd726930e4e20a964cc43ca3a9
Taryn Sauer <taryn.sauer@gmail.com>
2013-12-16 23:06:09 -0600

    Update for new gem

--- cli_ttt.rb
+++ cli_ttt.rb
@@ -1,5 +1,6 @@
 require 'ruby_ttt'
+
 board = Board.new(3)
 player_one = Player.new(MARKER_X)
 player_two = Player.new(MARKER_O)
-GameSetup.new(board, player_one, player_two).start!
+CLIGame.new(board, player_one, player_two).start_cli_game!
